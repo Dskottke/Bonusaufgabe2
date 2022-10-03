d7cd91ebd1e9d612d8c28b6f31cfcdfcee9bcf4d
davidskottke <davidskottke@googlemail.com>
2022-10-03 12:31:02 +0200

    add Method getNumberString add Tests for getNumberString

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,35 @@
+import java.util.stream.IntStream;
+/*
+Fülle ein String-Array mit den Zahlenwörtern "Eins" bis "Zehn"
+Schreibe eine Methode die zu einer einstelligen Zahl das entsprechende Zahlenwort ausgibt.
+Schreibe ein Programm das die Zahlen 1-100 ausgibt, aber:
+Vor Zahlen die durch 3 teilbar sind (modulo) soll ein # stehen
+Vor Zahlen die durch 5 teilbar sind soll ein $ stehen
+Schreibe ein Programm die Wörter vom Benutzer einliest (Scanner) und diese in einem Array speichert.
+Gib vor jeder Eingabe jeweils die eingegebenen Wörter noch mal aus.
+ */
 public class Main {
+    public static int[] valueArray = IntStream.range(1, 9).toArray();
+    public static String[] numberArray = {"one", "two", "three", "four"
+            , "five", "six", "seven", "eight", "nine",  };
+
     public static void main(String[] args) {
+        System.out.println(getNumberString(10));
 
     }
+
+    public static String getNumberString(int value) {
+
+
+        int indexNumber = 1;
+        for (String numberString : numberArray) {
+
+            if (indexNumber == value) {
+                return numberString;
+            } else
+                indexNumber++;
+        }
+
+       return "error";
 }
+                }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,5 +1,109 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class MainTest {
+    @Test
+    public void tenIsError(){
+        //given
+        int number = 10;
+        //when
+        String actual = Main.getNumberString(number);
+        //then
+        assertEquals("error",actual);
+    }
+    @Test
+    public void nullIsError(){
+        //given
+        int number = 0;
+        //when
+        String actual = Main.getNumberString(number);
+        //then
+        assertEquals("error",actual);
+    }
+    @Test
+    public void oneIs1(){
+        //given
+        int number = 1;
+        //when
+        String actual = Main.getNumberString(number);
+        //then
+        assertEquals("one",actual);
+    }
+    @Test
+    public void twoIs2(){
+        //given
+        int number = 2;
+        //when
+        String actual = Main.getNumberString(number);
+        //then
+        assertEquals("two",actual);
+    }
+    @Test
+    public void threeIs3(){
+        //given
+        int number = 3;
+        //when
+        String actual = Main.getNumberString(number);
+        //then
+        assertEquals("three",actual);
+    }
+    @Test
+    public void fourIs4(){
+        //given
+        int number = 4;
+        //when
+        String actual = Main.getNumberString(number);
+        //then
+        assertEquals("four",actual);
+    }
+    @Test
+    public void fiveIs5(){
+        //given
+        int number = 5;
+        //when
+        String actual = Main.getNumberString(number);
+        //then
+        assertEquals("five",actual);
+    }
+    @Test
+    public void SixIs6(){
+        //given
+        int number = 6;
+        //when
+        String actual = Main.getNumberString(number);
+        //then
+        assertEquals("six",actual);
+    }
+    @Test
+    public void SevenIs7(){
+        //given
+        int number = 7;
+        //when
+        String actual = Main.getNumberString(number);
+        //then
+        assertEquals("seven",actual);
+    }
+    @Test
+    public void eightIs8(){
+        //given
+        int number = 8;
+        //when
+        String actual = Main.getNumberString(number);
+        //then
+        assertEquals("eight",actual);
+    }
+    @Test
+    public void nineIs9(){
+        //given
+        int number = 9;
+        //when
+        String actual = Main.getNumberString(number);
+        //then
+        assertEquals("nine",actual);
+    }
+
+
 
 }
